4d131dfeb09baab97ea9b2b08e8883395f4af17a
yyeonzu <yyeonzu@gmail.com>
2025-07-09 16:55:12 +0900

    헤더 영역 html 뼈대 작성

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+export function createHeader() {
+    const header = document.createElement('header');
+    header.className ='flex-row-between';
+    header.innerHTML = `
+        <h1>Wise Wallet</h1>
+        <div class="flex-column-between">
+            <p>2023</p>
+            <div class="flex-row-center header-calendar-bar">
+                <a href="#"><</a>
+                <p>8</p>
+                <a href="#">></a>
+            </div>
+            <p>August</p>
+        </div>
+        <ul class="flex-row-center">
+            <li>
+            <a href="#">main</a>
+            </li>
+            <li>
+                <a href="#">calendar</a>
+            </li>
+            <li>
+                <a href="#">graph</a> 
+            </li>
+        </div>`;
+    return header;
+}
