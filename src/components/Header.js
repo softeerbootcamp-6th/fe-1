2d3c4b38931b5727d970146586cf7ccb17d4e209
minjoengss <dotoriido@gmail.com>
2025-07-09 16:50:21 +0900

    feat: 헤더 요소 배치 및 스타일 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,38 @@ import { navigate } from "../router.js";
 
 export const renderHeader = () => {
   const header = document.createElement("header");
+  header.id = "header";
 
   // logo
   const logo = document.createElement("p");
+  logo.classList.add("logo");
   logo.textContent = "Wise Wallet";
   header.appendChild(logo);
 
   // date
   const date = document.createElement("div");
+  date.classList.add("date");
   const year = "2023";
   const month = "8";
   const monthEng = "August";
   date.innerHTML = `
-    <span class="year">${year}</span>
-    <span class="month_num">${month}</span>
-    <span class="month_eng">${monthEng}</span>
     <div class="arrow"><</div>
-    <div class="arrow"><</div>
+    <div class="currentDate">
+        <span >${year}</span>
+        <span class="month_num">${month}</span>
+        <span >${monthEng}</span>
+    </div>
+    <div class="arrow">></div>
   `;
   header.appendChild(date);
 
   // navigation
   const nav = document.createElement("ul");
+  nav.classList.add("nav");
   const navItems = ["doc", "calendar", "chart"];
   navItems.map((item) => {
     const li = document.createElement("li");
     li.innerHTML = `<img src="./src/assets/${item}.png" alt="${item} icon" />`;
-    li.classList.add("nav_item");
     li.addEventListener("click", () => {
       navigate(item);
     });
