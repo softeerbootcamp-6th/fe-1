098879c613fc47b3b64447f1267089cdf09ab844
bobob0311 <qweasdzxc0311@naver.com>
2025-07-09 16:20:20 +0900

    header component 기본 뼈대 생성

---
+++ components/header/index.js
@@ -0,0 +1,49 @@
+export default function addHeaderHTML() {
+    const $header = document.getElementById('header-placeholder');
+
+    $header.innerHTML += `
+    <div id="header-wrapper">
+        <h1 id="main-title">Wise Wallet</h1>
+        <div id="center-wrrapper">
+            <button>
+                <img
+                    aria-label="왼쪽 버튼"
+                    src="public/chevron-left.svg"
+                />
+            </button>
+            <div id="location">
+                <span>2023</span>
+                <span>8</span>
+                <span>August</span>
+            </div>
+            <button>
+                <img
+                    aria-label="오른쪽 버튼"
+                    src="/public/chevron-right.svg"
+                />
+            </button>
+        </div>
+        <div id="header-tab">
+            <ul>
+                <li>
+                    <a
+                        ><img aria-label="내역" src="/public/doc.svg"
+                    /></a>
+                </li>
+                <li>
+                    <a
+                        ><img
+                            aria-label="달력"
+                            src="/public/calendar.svg"
+                    /></a>
+                </li>
+                <li>
+                    <a
+                        ><img aria-label="통계" src="public/chart.svg"
+                    /></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    `;
+}
