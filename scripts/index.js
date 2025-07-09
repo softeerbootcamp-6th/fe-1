7791221b87bcbd578a2dea52be771dfec28c027f
jjamming <ohj3694@naver.com>
2025-07-09 16:47:54 +0900

    feat: 날짜 증가/감소 버튼 함수 구현

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 import { loadPage } from "./router.js";
-
+import { initializeHeader
+ } from "./header.js";
 window.addEventListener('DOMContentLoaded', async () => {
     const headerEl = document.getElementById("header");
 
     const res = await fetch("./components/header.html");
     const html = await res.text();
     headerEl.innerHTML = html;
-    
+    initializeHeader();
     await loadPage();
 })
 
