62c840b2c79302f59cf97cbd0f23e49957be4499
Dodolist <dodolist3@gmail.com>
2025-07-10 13:39:33 +0900

    refactor: separate magic number in content input item

--- components/InputBar/InputItems/content.js
+++ components/InputBar/InputItems/content.js
@@ -1,3 +1,5 @@
+const MAX_LENGTH = 32;
+
 const createContent = () => {
     const contentItem = document.createElement('div');
     contentItem.className = 'input-bar-item';
@@ -6,13 +8,13 @@ const createContent = () => {
             <label for="content" class="light-12">내용</label>
             <div class="content-length">
                 <span class="light-12 content-length-count">0</span>
-                <span class="light-12">/32</span>
+                <span class="light-12">/${MAX_LENGTH}</span>
             </div>
         </div>
         <div class="input-bar-item-wrapper">
             <input
                 type="text"
-                maxlength="32"
+                maxlength=${MAX_LENGTH}
                 id="content"
                 class="semibold-12"
                 placeholder="입력하세요"
