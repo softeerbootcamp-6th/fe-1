7186d2b128e10f58d1664311331b9644691dc2c7
TaeSeung Yoo <remicon99@gmail.com>
2025-07-10 16:57:22 +0900

    fix: CategoryTag 컴포넌트 export 추가

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import TextInput from "./TextInput/TextInput.js";
 import CheckBox from "./CheckBox/CheckBox.js";
 import Modal from "./Modal/Modal.js";
 import Header from "./Header/Header.js";
+import CategoryTag from "./CategoryTag/CategoryTag.js";
 
-export { Button, TextInput, CheckBox, Modal, Header };
+export { Button, TextInput, CheckBox, Modal, Header, CategoryTag };
