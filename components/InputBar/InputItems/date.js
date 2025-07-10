0167033e22da902096de8bb7877a090e3fff9ffa
Dodolist <dodolist3@gmail.com>
2025-07-10 15:18:47 +0900

    feat: set default date input value to today

--- components/InputBar/InputItems/date.js
+++ components/InputBar/InputItems/date.js
@@ -1,4 +1,8 @@
+import { formatDate } from '../../../lib/utils.js';
+
 const createDate = () => {
+    const today = formatDate(new Date(), 'YYYY-MM-DD');
+
     const dateItem = document.createElement('div');
     dateItem.className = 'input-bar-item';
     dateItem.innerHTML = `
@@ -6,7 +10,7 @@ const createDate = () => {
             <label for="date-input" class="light-12">일자</label>
         </div>
         <div class="input-bar-item-wrapper">
-            <input type="date" id="date-input" />
+            <input type="date" id="date-input" value=${today} />
         </div>
     `;
 
