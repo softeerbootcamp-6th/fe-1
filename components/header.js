0fff6fd83f24f8a306c1788ad093f2d6abb03694
eunjin11 <whdmswls9207@gmail.com>
2025-07-10 16:20:13 +0900

    feat: <div id=header-container> -> <header id=header-container>로 변경: 렌더링 된 html을 확인했을 때 div 내부에 header이 있는 것 보다는 body의 자식으로 바로 header이 있는 것이 가독성과 접근성 측면에서 낫다고 판단해 변경했습니다.

--- components/header.js
+++ components/header.js
@@ -1,24 +1,60 @@
 export function createHeader() {
   const headerTemplate = `
-    <header>
-      <h1 class="serif-24">Wise Wallet</h1>
-      <div class="flex-column">
-        <div class="currentYear light-14">2023</div>
-        <div class="flex-row">
-          <button id="prevMonth" class="light-14"><</button>
-          <div id="currentMonth" class="serif-48">8</div>
-          <button id="nextMonth" class="light-14">></button>
-        </div>
-        <div id="currentMonthName" class="light-14">August</div>
+  <h1 class="serif-24">Wise Wallet</h1>
+  <div class="flex-column">
+    <div class="currentYear light-14">2023</div>
+    <div class="flex-row">
+      <button
+        id="prevMonth"
+        class="light-14"
+      >
+        <
+      </button>
+      <div
+        id="currentMonth"
+        class="serif-48"
+      >
+        8
       </div>
-      <nav>
-        <ul class="flex-row">
-          <li><a href="/"><img src="../icons/doc.svg" alt="main page" /></a></li>
-          <li><a href="/calendar"><img src="../icons/calendar.svg" alt="calendar" /></a></li>
-          <li><a href="/chart"><img src="../icons/chart.svg" alt="chart" /></a></li>
-        </ul>
-      </nav>
-    </header>
+      <button
+        id="nextMonth"
+        class="light-14"
+      >
+        >
+      </button>
+    </div>
+    <div
+      id="currentMonthName"
+      class="light-14"
+    >
+      August
+    </div>
+  </div>
+  <nav>
+    <ul class="flex-row">
+      <li>
+        <a href="/"
+          ><img
+            src="../icons/doc.svg"
+            alt="main page"
+        /></a>
+      </li>
+      <li>
+        <a href="/calendar"
+          ><img
+            src="../icons/calendar.svg"
+            alt="calendar"
+        /></a>
+      </li>
+      <li>
+        <a href="/chart"
+          ><img
+            src="../icons/chart.svg"
+            alt="chart"
+        /></a>
+      </li>
+    </ul>
+  </nav>
   `;
 
   return headerTemplate;
