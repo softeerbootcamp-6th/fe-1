e578a2ec2c6790151373cb767b21bb70fb13a79c
bobob0311 <qweasdzxc0311@naver.com>
2025-07-11 14:58:55 +0900

    summit button 구독 및 전체 valid 설정 (변수 개개인 유효성 로직은 아직.)

--- store/formData.js
+++ store/formData.js
@@ -7,24 +7,50 @@ const formData = {
     createDate: null,
     isValid: false,
 
+    isValidListeners: new Set(),
+
     setDate(dateValue) {
         this.date = dateValue;
+        this.checkAndNotify();
     },
     setAmount(amountValue) {
         this.amount = amountValue;
+        this.checkAndNotify();
     },
     setDescription(descriptionValue) {
         this.description = descriptionValue;
+        this.checkAndNotify();
     },
     setPayment(paymentValue) {
         this.payment = paymentValue;
+        this.checkAndNotify();
     },
     setCategory(categoryValue) {
         this.category = categoryValue;
+        this.checkAndNotify();
     },
 
     checkValid() {
-        return this.date && this.amount && this.description && this.category;
+        this.isValid =
+            this.date && this.amount && this.description && this.category;
+    },
+
+    subscribeIsValid(listener) {
+        this.isValidListeners.add(listener);
+        return () => this._isValidListeners.delete(listener);
+    },
+
+    checkAndNotify() {
+        const prev = this.isValid;
+        this.isValid = !!(
+            this.date &&
+            this.amount &&
+            this.description &&
+            this.category
+        );
+
+        if (prev != this.isValid)
+            this.isValidListeners.forEach((fn) => fn(this.isValid, this));
     },
 };
 
