b5e58931423cf105c036de417af6903eaec60edb
choi-jiae <chlwldo2002@gmail.com>
2025-07-11 12:05:41 +0900

    [feat] dateState를 header, IncomeExpenseList에 적용
    - header에서 월 조작 가능
    - header에서 월 변경할 경우 IncomeExpenseList에 반영

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import { renderHeader } from "./components/Header.js";
+import { renderIncomeExpenseList } from "./components/IncomeExpenseList.js";
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
   const header = renderHeader();
+  const incomeExpenseList = renderIncomeExpenseList();
+  
   app.appendChild(header);
+  app.appendChild(incomeExpenseList);
 });
