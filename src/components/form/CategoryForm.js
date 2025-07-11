5fb8e31f52e41ee51df598adda61394e1246700e
minjoengss <dotoriido@gmail.com>
2025-07-11 13:45:15 +0900

    feat: 드롭다운 열림/닫힘 여부 통제 구현

--- src/components/form/CategoryForm.js
+++ src/components/form/CategoryForm.js
@@ -1,9 +1,23 @@
-export const CategoryForm = () => {
+export const CategoryForm = (input) => {
+  let isCategoryOpen = false;
   const categoryForm = document.createElement("div");
   categoryForm.classList.add("form-category");
   categoryForm.innerHTML = `
         <label for="category" class="light-12">카테고리</label>
-        <input type="text" id="category" name="category" placeholder="입력하세요">
+        <button id="category">
+          <span class="semibold-12">${input.category}</span>
+          <img width="16px" src="./src/assets/chevron-${
+            isCategoryOpen ? "up" : "down"
+          }.png" alt="arrow"/>
+        </button>
     `;
+
+  const categoryImg = categoryForm.querySelector("#category > img");
+  categoryForm.addEventListener("click", () => {
+    isCategoryOpen = !isCategoryOpen;
+    categoryImg.src = `./src/assets/chevron-${
+      isCategoryOpen ? "up" : "down"
+    }.png`;
+  });
   return categoryForm;
 };
--- src/components/form/EntireForm.js
+++ src/components/form/EntireForm.js
@@ -14,7 +14,7 @@ export const EntireForm = () => {
     money: "",
     content: "",
     payment: "",
-    category: "",
+    category: "aa",
   };
 
   const formList = [
