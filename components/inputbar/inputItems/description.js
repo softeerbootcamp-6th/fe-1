87a8676e0e1695bba978e3cfaec92860d9ef6f55
bobob0311 <qweasdzxc0311@naver.com>
2025-07-11 11:39:56 +0900

    내용 inputbox 글자수 세기 기능 추가

--- components/inputbar/inputItems/description.js
+++ components/inputbar/inputItems/description.js
@@ -3,9 +3,10 @@ import formData from '../../../store/formData.js';
 
 export default function () {
     const descriptionInputInnerHtml = `
-            <label for="descriptionInput" class="lt-12">
-                내용
-            </label>
+            <span class="count-box-header">
+                <label for="descriptionInput" class="lt-12"> 내용 </label>
+                <span class="count-box lt-12"> <span id="current-text-length"> 0</span>/32</span>
+            </span>
             <input
                 placeholder="입력하세요"
                 id="descriptionInput"
@@ -22,6 +23,15 @@ export default function () {
         },
         descriptionInputInnerHtml,
     );
+
+    const $input = $descriptionInputItem.querySelector('#descriptionInput');
+    const $currentLength = $descriptionInputItem.querySelector(
+        '#current-text-length',
+    );
+
+    $input.addEventListener('input', (e) => {
+        $currentLength.textContent = e.target.value.length;
+    });
 
     const $descriptionInput =
         $descriptionInputItem.querySelector('#descriptionInput');
