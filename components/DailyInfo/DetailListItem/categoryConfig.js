ce65e8e8aaab54f062987eb7060ff1c4be50acc3
Dodolist <dodolist3@gmail.com>
2025-07-11 11:55:33 +0900

    chore: create category config file

---
+++ components/DailyInfo/DetailListItem/categoryConfig.js
@@ -0,0 +1,44 @@
+const categoryConfig = {
+    living: {
+        text: '생활',
+        color: 'colorchip-90',
+    },
+    shopping: {
+        text: '쇼핑/뷰티',
+        color: 'colorchip-30',
+    },
+    health: {
+        text: '의료/건강',
+        color: 'colorchip-50',
+    },
+    food: {
+        text: '식비',
+        color: 'colorchip-60',
+    },
+    transport: {
+        text: '교통',
+        color: 'colorchip-70',
+    },
+    leisure: {
+        text: '문화/여가',
+        color: 'colorchip-100',
+    },
+    uncategorized: {
+        text: '미분류',
+        color: 'colorchip-110',
+    },
+    salary: {
+        text: '월급',
+        color: 'colorchip-20',
+    },
+    other_income: {
+        text: '기타 수입',
+        color: 'colorchip-10',
+    },
+    allowance: {
+        text: '용돈',
+        color: 'colorchip-40',
+    },
+};
+
+export default categories;
