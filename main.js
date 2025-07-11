5fe574d996329de60131d9234ed4e01fe120a120
choi-jiae <chlwldo2002@gmail.com>
2025-07-11 14:47:49 +0900

    [style] 지출수입내역 입력폼 구현중
    - 지출수입내역 입력폼 html 뼈대 만들기
    - 지출수입내역 입력 폼 CSS 적용중

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import { renderHeader } from "./components/Header.js";
 import { renderIncomeExpenseList } from "./components/IncomeExpenseList.js";
-
+import { renderIncomeExpenseForm } from "./components/IncomeExpenseForm.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
   const header = renderHeader();
   const incomeExpenseList = renderIncomeExpenseList();
+  const incomeExpenseForm = renderIncomeExpenseForm();
   
   app.appendChild(header);
   app.appendChild(incomeExpenseList);
+  app.appendChild(incomeExpenseForm);
 });
