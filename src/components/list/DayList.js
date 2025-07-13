4f3dda7e72fa142a7dbbe735b4f0232e0e3db8f8
minjoengss <dotoriido@gmail.com>
2025-07-14 02:40:10 +0900

    feat: 내역 영역 각 날짜별 리스트 레이아웃 구현

--- src/components/list/DayList.js
+++ src/components/list/DayList.js
@@ -3,8 +3,33 @@ import { ListItem } from "./ListItem.js";
 
 export const DayList = (inputs) => {
   const dayList = ElementManager.renderElement("div", "list-day");
+  const dayOverview = ElementManager.renderElement("div", [
+    "day-overview",
+    "serif-14",
+  ]);
+  dayOverview.innerHTML = `
+  <div class="date">
+    <span>8월 14일</span>
+    <span>월요일</span>
+  </div>
+  <div class="money-counter">
+    <div>
+        <span>수입</span>
+        <span>0원</span>
+    </div>
+    <div>
+        <span>지출</span>
+        <span>0원</span>
+    </div>
+  </div>
+  `;
+  dayList.appendChild(dayOverview);
+
+  const dayWrapper = ElementManager.renderElement("div", "day-wrapper");
+
   inputs.map((input) => {
-    dayList.appendChild(ListItem(input));
+    dayWrapper.appendChild(ListItem(input));
   });
+  dayList.appendChild(dayWrapper);
   return dayList;
 };
