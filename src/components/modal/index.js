921016e5382449bc2d3a749278ccf47524ac5296
minjoengss <dotoriido@gmail.com>
2025-07-13 18:36:02 +0900

    feat: Modal 레이아웃 구현

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,9 +1,45 @@
 import { ElementManager } from "../../utils/ElementManager.js";
 
-export const Modal = {
-  renderModal: () => {
-    const modal = ElementManager.renderElement("div", "modal");
-    return modal;
+const COMMENT_TYPE = {
+  add: {
+    label: "추가하실 결제 수단을 입력해주세요",
+    button: "추가",
   },
-  hideModal: () => {},
+  delete: {
+    label: "해당 결제 수단을 삭제하시겠습니까?",
+    button: "삭제",
+  },
+};
+
+export const Modal = {
+  renderModal: (type) => {
+    const modal = ElementManager.renderElementId("div", "modal");
+    modal.innerHTML = `
+    <div class="modal-wrapper">
+      <div class="input-container">
+        <label class="light-16" for="modal-input">${COMMENT_TYPE[type].label}</label>
+        <input class="semibold-12" id="modal-input" placeholder="placehoder"/>
+      </div>
+      <div class="button-container">
+        <button class="cancel semibold-16">취소</button>
+        <button class="confirm semibold-16">${COMMENT_TYPE[type].button}</button>
+      </div>
+    </div>
+    `;
+    document.body.appendChild(modal);
+
+    const buttonContainer = modal.querySelector(".button-container");
+    buttonContainer.addEventListener("click", (e) => {
+      if (e.target.closest(".cancel")) {
+        Modal.hideModal();
+      }
+      if (e.target.closest(".confirm")) {
+        Modal.hideModal();
+      }
+    });
+  },
+  hideModal: () => {
+    const modal = document.body.querySelector("#modal");
+    document.body.removeChild(modal);
+  },
 };
