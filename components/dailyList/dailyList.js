645b5bee6c3962fd742ba3ee84b0a94643de0b8c
bobob0311 <qweasdzxc0311@naver.com>
2025-07-14 17:44:50 +0900

    수입 지출 추가

--- components/dailyList/dailyList.js
+++ components/dailyList/dailyList.js
@@ -6,8 +6,9 @@ export default function createDaliyList(dailyInfo) {
     const dateToKorean = formatDateToKorean(date);
 
     const dailyListContainerInnerHtml = `
-        <div class="date-info">${dateToKorean}</div>
-        <div></div>
+        <div class="daily-header">
+            <div class="date-info">${dateToKorean}</div>
+        </div>
         <ol class="daliy-line-wrapper"></ol>
     `;
 
@@ -23,9 +24,30 @@ export default function createDaliyList(dailyInfo) {
         '.daliy-line-wrapper',
     );
 
+    let totalIncome = 0;
+    let totalExpense = 0;
+
     dailyInfo.items.forEach((info) => {
         $dailyListWrapper.appendChild(createDaily(info));
+        console.log(info);
+        if (info.amount > 0) {
+            totalIncome += info.amount;
+        } else {
+            totalExpense -= info.amount;
+        }
     });
+
+    let amountHtml = '';
+    amountHtml += totalIncome != 0 ? `<div>수입 ${totalIncome}<div>` : '';
+    amountHtml += totalExpense != 0 ? `<div>지출 ${totalExpense}<div>` : '';
+
+    const $dailyAmount = createElement(
+        'div',
+        { class: 'amount-wrapper' },
+        amountHtml,
+    );
+
+    $dailyListContainer.querySelector('.date-info').after($dailyAmount);
 
     return $dailyListContainer;
 }
