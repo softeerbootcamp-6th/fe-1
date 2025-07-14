4c50ac735b19cb7030f8175b705701c58493a02d
Kim Minjeong <dotoriido@gmail.com>
2025-07-14 23:05:36 +0900

    refactor: (코드리뷰 반영) date 분할 방법 구조 분해 할당으로 변경
    Co-authored-by: Jongwoo Lee <dodolist3@gmail.com>

--- src/components/list/DateList.js
+++ src/components/list/DateList.js
@@ -3,7 +3,7 @@ import { ElementManager } from "../../utils/ElementManager.js";
 import { ListItem } from "./ListItem.js";
 
 export const DateList = (date, list) => {
-  const splitDate = date.split("-");
+  const [year, month, day] = date.split("-");
   const dateList = ElementManager.renderElement("div", "list-date");
   const dateOverview = ElementManager.renderElement("div", [
     "date-overview",
@@ -11,7 +11,7 @@ export const DateList = (date, list) => {
   ]);
   dateOverview.innerHTML = `
   <div class="date">
-    <span>${splitDate[1]}월 ${splitDate[2]}일</span>
+    <span>${month}월 ${day}일</span>
     <span>${DateManager.renderDay(date)}요일</span>
   </div>
   <div class="money-counter">
