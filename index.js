e4f195da2bedb207e62c40464456e9d596da4f10
bobob0311 <qweasdzxc0311@naver.com>
2025-07-14 15:58:35 +0900

    dailyList DOM에 부착 및 초기화 함수 호출

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import initalizeDailyList from './components/dailyList/index.js';
 import initializeHeader from './components/header/index.js';
 import initalizeInputBox from './components/inputbar/index.js';
+import { dailyData } from './store/daily.js';
 import dateData from './store/date.js';
 
+await dailyData.init();
 initializeHeader();
 initalizeInputBox();
+initalizeDailyList();
 
 dateData.initDateData();
