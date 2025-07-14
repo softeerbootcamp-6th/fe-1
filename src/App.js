fac2c5a56f08e45618c7b3a21588ebc5db975fc1
bamb14 <althcjstk08@gmail.com>
2025-07-14 18:35:13 +0900

    feat: App.js에서 init 실행 시 dateStore 초기화

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import Header from './layout/Header.js';
 import MainPage from './pages/MainPage/MainPage.js';
+import dateStore from '../store/dateStore.js';
 
 function App() {
+    const header = Header();
     const mainPage = MainPage();
-    
+
+
     return {
         element: `
-            ${Header().element}
+            ${header.element}
             ${mainPage.element}
         `,
         init: () => {
+            // 날짜 데이터 초기화
+            if (dateStore.initDateData) {
+                dateStore.initDateData();
+            }
+
+            // Header 초기화
+            if (header.init) {
+                header.init();
+            }
+
             // MainPage 초기화
             if (mainPage.init) {
                 mainPage.init();
             }
+
+
+
+
         }
     }
 }
