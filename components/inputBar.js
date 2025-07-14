a1d5d773d8fe72e8acde0d3b688cf1f3175f7855
eunjin11 <whdmswls9207@gmail.com>
2025-07-14 14:46:15 +0900

    feat: amountToggle 이미지 추가

--- components/inputBar.js
+++ components/inputBar.js
@@ -4,59 +4,83 @@ import { renderTransactionList } from "./transactionsList.js";
 
 export function createInputBar() {
   return ` 
-    <form class="input-bar flex-row" id="inputBarForm">
-      <div class="flex-column">
-        <label>일자
-          <input type="date" name="date" required value="${
-            new Date().toISOString().split("T")[0]
-          }" />
-        </label>
+  <form
+    class="input-bar flex-row"
+    id="inputBarForm"
+  >
+    <label class="flex-column">
+      <div class="input-label light-12">일자</div>
+      <input type="date" name="date" class="date-input semibold-12" required value="${
+        new Date().toISOString().split("T")[0]
+      }" />
+    </label>
+    <label class="flex-column">
+      <div class="input-label light-12">금액</div>
+      <div class="flex-row semibold-12">
+        <button
+          type="button"
+          class="amountToggle"
+          data-sign="+"
+        >
+          <img src="../icons/plus.svg" alt="plus" />
+        </button>
+        <input
+          type="number"
+          name="amount"
+          placeholder="금액을 입력하세요"
+          min="0"
+          required
+        />
       </div>
-      <div class="flex-column">
-        <label>금액
-          <div class="flex-row">
-            <button type="button" class="amountToggle" data-sign="+">+</button>
-            <input type="number" name="amount" placeholder="금액을 입력하세요" min="0" required />
-          </div>
-        </label>
-      </div>
-      <div class="flex-column">
-        <label>내용
-          <input
-            type="text"
-            name="content"
-            maxlength="32"
-            placeholder="내용을 입력하세요"
-            required
-          />
-        </label>
-      </div>
-      <div class="flex-column">
-        <label>결제수단
-          <select name="paymentMethod" required>
-            <option value="cash">현금</option>
-            <option value="card">카드</option>
-            <option value="add">추가하기</option>
-          </select>
-        </label>
-      </div>
-      <div class="flex-column">
-        <label>분류
-        <select name="category" required>
-          <option value="life">생활</option>
-          <option value="food">식비</option>
-          <option value="transportation">교통</option>
-          <option value="shopping">쇼핑/뷰티</option>
-          <option value="health">의료/건강</option>
-          <option value="entertainment">문화/여가</option>
-          <option value="etc">미분류</option>
-        </select>
-        </label>
-      </div>
-      <div class="flex-column">
-        <button type="submit" class="add-button">추가</button>
-      </div>
-    </form>
+    </label>
+    <label class="flex-column">
+      <div class="input-label light-12">내용</div>
+      <input
+        type="text"
+        name="content"
+        maxlength="32"
+        placeholder="내용을 입력하세요"
+        required
+        class="semibold-12"
+      />
+    </label>
+    <label class="flex-column">
+      <div class="input-label light-12">결제수단</div>
+      <select
+        name="paymentMethod"
+        required
+        class="semibold-12"
+      >
+        <option value="cash">현금</option>
+        <option value="card">카드</option>
+        <option value="add">추가하기</option>
+      </select>
+    </label>
+    <label class="flex-column">
+      <div class="input-label light-12">분류</div>
+      <select
+        name="category"
+        required
+        class="semibold-12"
+      >
+        <option value="life">생활</option>
+        <option value="food">식비</option>
+        <option value="transportation">교통</option>
+        <option value="shopping">쇼핑/뷰티</option>
+        <option value="health">의료/건강</option>
+        <option value="entertainment">문화/여가</option>
+        <option value="etc">미분류</option>
+      </select>
+    </label>
+    <div class="flex-column">
+      <button
+        type="submit"
+        class="add-button"
+      >
+        추가
+      </button>
+    </div>
+  </form>
   `;
 }
 
@@ -69,12 +93,13 @@ export function renderInputBar(container) {
 
   if (amountToggle && amountInput) {
     amountToggle.addEventListener("click", () => {
+      console.log("amountToggle", amountToggle.dataset.sign);
       //+ - 변경
       if (amountToggle.dataset.sign === "+") {
-        amountToggle.textContent = "-";
+        amountToggle.innerHTML = `<img src="../icons/minus.svg" alt="minus" />`;
         amountToggle.dataset.sign = "-";
       } else {
-        amountToggle.textContent = "+";
+        amountToggle.innerHTML = `<img src="../icons/plus.svg" alt="plus" />`;
         amountToggle.dataset.sign = "+";
       }
     });
@@ -97,7 +122,7 @@ export function renderInputBar(container) {
     addNewTransaction(getCurrentYear(), getCurrentMonth(), data);
 
     form.reset();
-    amountToggle.textContent = "+";
+    amountToggle.innerHTML = `<img src="../icons/plus.svg" alt="plus" />`;
 
     renderTransactionList();
   });
