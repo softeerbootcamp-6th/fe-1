8fff33e10e0592866cba37a27d379125be79d5a3
TaeSeung Yoo <remicon99@gmail.com>
2025-07-14 17:41:46 +0900

    feat: 홈화면 수정 (모듈화 및 리팩토링 필요)

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,8 +3,126 @@ import {
   CheckBox,
   CategoryTag,
   DailyHistory,
+  Select,
 } from "../../components/index.js";
 import { getCurrentMonth } from "../../utils/month.js";
+
+const inputData = {
+  date: new Date().toISOString().split("T")[0],
+  type: "expenses", // income, expenses
+  amount: 0,
+  description: "",
+  method: "",
+  category: "",
+};
+
+console.log(inputData);
+
+const renderHeader = () => {
+  const header = Header({
+    selectedNav: "home",
+    onChangeMonth: renderHistory,
+  });
+  document.getElementById("header-container").appendChild(header);
+};
+
+const renderInputForm = async () => {
+  const dateInput = document.querySelector(".input-form__date-input");
+  const amountInput = document.querySelector(".input-form__amount-input");
+  const descriptionInput = document.querySelector(
+    ".input-form__description-input"
+  );
+  const descriptionCount = document.querySelector(
+    ".input-form__description-count"
+  );
+
+  dateInput.value = inputData.date;
+  amountInput.value = inputData.amount;
+  descriptionInput.value = inputData.description;
+
+  dateInput.addEventListener("change", (e) => {
+    inputData.date = e.target.value;
+  });
+
+  amountInput.addEventListener("input", (e) => {
+    const numericValue = e.target.value.replace(/[^\d,]/g, "");
+    const amount = Number(numericValue.replace(/,/g, ""));
+
+    if (!isNaN(amount)) {
+      inputData.amount = amount;
+      amountInput.value = amount.toLocaleString() || "";
+    } else {
+      // 잘못된 입력인 경우 이전 값 유지
+      amountInput.value = inputData.amount.toLocaleString() || "";
+    }
+  });
+
+  descriptionInput.addEventListener("input", (e) => {
+    inputData.description = e.target.value;
+    descriptionCount.textContent = `${e.target.value.length}/32`;
+  });
+
+  const inputAmountIcon = document.querySelector(".input-form__amount-icon");
+  inputAmountIcon.addEventListener("click", () => {
+    if (inputAmountIcon.classList.contains("input-form__amount-icon--minus")) {
+      inputAmountIcon.classList.remove("input-form__amount-icon--minus");
+      inputAmountIcon.classList.add("input-form__amount-icon--plus");
+      inputData.type = "income";
+    } else {
+      inputAmountIcon.classList.remove("input-form__amount-icon--plus");
+      inputAmountIcon.classList.add("input-form__amount-icon--minus");
+      inputData.type = "expenses";
+    }
+  });
+
+  const methodSelectContainer = document.getElementById(
+    "method-select-container"
+  );
+  const methodSelect = await Select({
+    label: "결제수단",
+    options: JSON.parse(localStorage.getItem("method")),
+    id: "method",
+    isEditable: true,
+    onSelect: (selectedOption) => {
+      inputData.method = selectedOption;
+    },
+  });
+
+  methodSelectContainer.appendChild(methodSelect);
+
+  const categorySelectContainer = document.getElementById(
+    "category-select-container"
+  );
+  const categoryObject = JSON.parse(localStorage.getItem("category"));
+  const categorySelect = await Select({
+    label: "분류",
+    options: categoryObject[inputData.type],
+    id: "category",
+    onSelect: (selectedOption) => {
+      inputData.category = selectedOption;
+    },
+  });
+
+  categorySelectContainer.appendChild(categorySelect);
+};
+
+const renderFilter = () => {
+  const filterContainer = document.getElementById("filter-container");
+  const incomeCheckBox = CheckBox({
+    id: "income-checkbox",
+    checked: true,
+    label: "수입 2,113,123",
+    onChange: renderHistory,
+  });
+  const expenseCheckBox = CheckBox({
+    id: "expense-checkbox",
+    checked: true,
+    label: "지출 2,113,123",
+    onChange: renderHistory,
+  });
+
+  filterContainer.append(incomeCheckBox, expenseCheckBox);
+};
 
 const renderHistory = () => {
   const isIncomeChecked = document.getElementById("income-checkbox").checked;
@@ -70,32 +188,7 @@ const renderHistory = () => {
   expenseLabel.textContent = `지출 ${totalExpense.toLocaleString()}원`;
 };
 
-const renderHeader = () => {
-  const header = Header({
-    selectedNav: "home",
-    onChangeMonth: renderHistory,
-  });
-  document.getElementById("header-container").appendChild(header);
-};
-
-const renderFilter = () => {
-  const filterContainer = document.getElementById("filter-container");
-  const incomeCheckBox = CheckBox({
-    id: "income-checkbox",
-    checked: true,
-    label: "수입 2,113,123",
-    onChange: renderHistory,
-  });
-  const expenseCheckBox = CheckBox({
-    id: "expense-checkbox",
-    checked: true,
-    label: "지출 2,113,123",
-    onChange: renderHistory,
-  });
-
-  filterContainer.append(incomeCheckBox, expenseCheckBox);
-};
-
 renderHeader();
+renderInputForm();
 renderFilter();
 renderHistory();
