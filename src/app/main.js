b04c068800fc616625017d267b5ade042a9e3c02
Park Jong Seong <pjsc0903@gmail.com>
2025-07-15 18:08:53 +0900

    기존의 store를 summaryStore로 변경 및 dateStore 추가

--- src/app/main.js
+++ src/app/main.js
@@ -1,24 +1,29 @@
-import { Store } from './store.js';
-import { initDateNav } from '../domains/calendar/ui/dateNav.js';
-import { initSummaryView } from '../domains/summary/ui/summaryView.js';
-import { initFormView } from '../domains/entryForm/ui/formView.js';
-import { loadDummyData } from '../shared/data/dummyData.js';
+import {Store} from './store.js';
+import {initDateNav} from '../domains/headerDate/ui/dateNav.js';
+import {initSummaryView} from '../domains/summary/ui/summaryView.js';
+import {initFormView} from '../domains/entryForm/ui/formView.js';
+import {loadDummyData} from '../shared/data/dummyData.js';
 
 const bootstrap = () => {
     // Store 생성
-    const store = new Store({ entries: [] });
+    const summaryStore = new Store({entries: []});
+    const dateStore = new Store({year: 2000, month: 9});
 
     // 날짜쪽 UI 초기화
-    initDateNav({ navEl: document.querySelector('.month-nav') });
-    // 달별 가계부 UI 초기화
-    initSummaryView({ summaryEl: document.querySelector('.month-summary'), store });
+    initDateNav({navEl: document.querySelector('.month-nav'), dateStore: dateStore});
+    // 월별 가계부 UI 초기화
+    initSummaryView({
+        summaryEl: document.querySelector('.month-summary'),
+        summaryStore: summaryStore,
+        dateStore: dateStore
+    });
     // 입력폼 UI 초기화
-    initFormView({ formEl: document.querySelector('.detail-form'), store });
+    initFormView({formEl: document.querySelector('.detail-form'), summaryStore: summaryStore});
 
     // 더미 데이터를 비동기(Promise) 방식으로 로드 후 스토어에 주입
     loadDummyData()
         .then(dummy => {
-            dummy.forEach(e => store.dispatch('ENTRY/ADD', e));
+            dummy.forEach(e => summaryStore.dispatch('ENTRY/ADD', e));
         });
 };
 
