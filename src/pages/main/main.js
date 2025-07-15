fd3db2f48f0f8da83865a311156cb20c2e0c31a4
홍규진 <hkj0206@dgu.ac.kr>
2025-07-15 17:49:30 +0900

    refactor: Main 페이지 내에서 불필요한 이벤트 리스너 추상화 및 전역 변수 제거

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,35 +1,33 @@
-import { accountBookStore } from "../../store/account-book-store.js";
 import {
   updateAmountSign,
   setupAmountInputListeners,
   setupContentInputListeners,
-} from "../../utils/formatUtils.js";
-import { updateHeaderDate, updateInputDate } from "../../utils/dateUtils.js";
+} from "../../utils/format-utils.js";
+import { updateHeaderDate, updateInputDate } from "../../utils/date-utils.js";
 import {
   setupGlobalVariables,
   setupGlobalFunctions,
-  setupStoreSubscription,
-  setupMainEventListeners,
+  setupToggleListeners,
+  setupDateInputListeners,
+  setupAddButtonListeners,
+  setupRealtimeValidation,
+  setupCustomDropdowns,
   initializeRendering,
   initializeModal,
-} from "./main-uiUtils.js";
+} from "./main-ui-utils.js";
 import { initModal } from "../../layouts/modal/modal.js";
 import {
-  validateFormData,
-  processAmountSign,
-  createNewItem,
   deleteItem,
   onMonthChanged,
   getFilteredData,
-} from "../../utils/dataUtils.js";
+} from "../../utils/data-utils.js";
 
 function initMain() {
   // 전역 변수들을 window 객체에 등록
-  const elements = setupGlobalVariables();
+  setupGlobalVariables();
 
   // 전역 함수들을 window 객체에 등록
   setupGlobalFunctions(
-    accountBookStore,
     deleteItem,
     updateHeaderDate,
     updateInputDate,
@@ -37,24 +35,17 @@ function initMain() {
     onMonthChanged
   );
 
-  // Store 변경 감지 설정
-  setupStoreSubscription(accountBookStore);
-
-  // 이벤트 리스너 설정
-  setupMainEventListeners(
-    elements,
-    updateAmountSign,
-    setupAmountInputListeners,
-    setupContentInputListeners,
-    updateHeaderDate,
-    validateFormData,
-    processAmountSign,
-    createNewItem,
-    deleteItem
-  );
+  // 각각의 이벤트 리스너 설정
+  setupToggleListeners();
+  setupDateInputListeners();
+  setupAmountInputListeners();
+  setupContentInputListeners();
+  setupAddButtonListeners();
+  setupRealtimeValidation();
+  setupCustomDropdowns();
 
   // 최초 렌더링
-  initializeRendering(accountBookStore, updateHeaderDate, updateInputDate);
+  initializeRendering(updateHeaderDate, updateInputDate);
 
   // 모달 초기화
   initializeModal(initModal);
