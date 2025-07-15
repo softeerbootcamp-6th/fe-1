96520b4d2405ed565ebb0bfed855c3ac613cbf6e
TaeSeung Yoo <remicon99@gmail.com>
2025-07-15 11:27:44 +0900

    feat: createElement 유틸 함수 생성

---
+++ src/utils/dom.js
@@ -0,0 +1,10 @@
+export const createElement = async (path) => {
+  const response = await fetch(path);
+  const template = await response.text();
+
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = template;
+  const element = tempDiv.firstElementChild;
+
+  return element;
+};
