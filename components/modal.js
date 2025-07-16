5d0b0693b57be10c5e7632e65a98e2801773ccf6
sobogil <sotomas232@gmail.com>
2025-07-16 18:33:49 +0900

    refactor : 두개 모달 하나로 만들고 코드 포맷 설정 후 코드 들여쓰기 수정

--- components/modal.js
+++ components/modal.js
@@ -1,36 +1,42 @@
-export function createModal({title, confirmText = "확인", cancelText = "취소", onConfirm, onCancel, children}) {
-    const modalContainer = document.createElement('div');
-    modalContainer.id = "modal";
-    modalContainer.className = "modal";
-    modalContainer.innerHTML = `
+export function createModal({
+  title,
+  confirmText = "확인",
+  cancelText = "취소",
+  onConfirm,
+  onCancel,
+  children,
+}) {
+  const modalContainer = document.createElement("div");
+  modalContainer.id = "modal";
+  modalContainer.className = "modal";
+  modalContainer.innerHTML = `
         <div class="modal-content">
             <h3>${title}</h3>
-            ${children || ''}
+            ${children || ""}
             <button id="confirm-btn" class="confirm-btn">${confirmText}</button>
             <button id="cancel-btn" class="cancel-btn">${cancelText}</button>
         </div>
-    `
+    `;
 
-    const confirmButton = modalContainer.querySelector('#confirm-btn');
-    const cancelButton = modalContainer.querySelector('#cancel-btn');
+  const confirmButton = modalContainer.querySelector("#confirm-btn");
+  const cancelButton = modalContainer.querySelector("#cancel-btn");
 
-    confirmButton.addEventListener('click', () => {
-        if (onConfirm) {
-            onConfirm();
-        }
-        modalContainer.remove();
-    });
+  confirmButton.addEventListener("click", () => {
+    if (onConfirm) {
+      onConfirm();
+    }
+    modalContainer.remove();
+  });
 
-    cancelButton.addEventListener('click', () => {
-        if (onCancel) {
-            onCancel();
-        }
-        modalContainer.remove();
-    });
+  cancelButton.addEventListener("click", () => {
+    if (onCancel) {
+      onCancel();
+    }
+    modalContainer.remove();
+  });
 
-    document.body.appendChild(modalContainer);
+  document.body.appendChild(modalContainer);
 }
-
 
 /*
  <div id="delete-modal" class="delete-modal delete-hidden">
