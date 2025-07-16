f9cac537377283621bd8dd9c9f6872c6f8901a84
Dodolist <dodolist3@gmail.com>
2025-07-16 18:33:18 +0900

    feat: create dropdown options delete modal

--- components/DropdownOption/index.js
+++ components/DropdownOption/index.js
@@ -1,5 +1,6 @@
 import { close } from '../../lib/utils.js';
 import formStore from '../../store/form.js';
+import createModal from '../Modal/index.js';
 
 const createDropdownOption = (option, deleteOption) => {
     const optionContainer = document.createElement('li');
@@ -24,14 +25,31 @@ const createDropdownOption = (option, deleteOption) => {
         </div>
     `;
 
+    const modal = createModal({
+        okText: '삭제',
+        onOk: () => {
+            optionContainer.remove();
+            formStore.deletePaymentMethod(option.value);
+        },
+        okTextColor: 'var(--danger-text-default)',
+        content: `
+            <span class="light-16">해당 결제 수단을 삭제하시겠습니까?</span>
+            <input
+                class="semibold-12 input-payment-method"
+                type="text"
+                disabled
+                placeholder="${option.label}"
+            />
+        `,
+    });
+
     if (deleteOption) {
         const deleteButton = optionContainer.querySelector(
             '.option-delete-button'
         );
         deleteButton.addEventListener('click', (event) => {
             event.stopPropagation();
-            optionContainer.remove();
-            formStore.deletePaymentMethod(option.value);
+            modal.open();
         });
     }
 
