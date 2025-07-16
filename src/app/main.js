eac8057ceb3435ca37a65eeec383301fffa0cd4c
Park Jong Seong <pjsc0903@gmail.com>
2025-07-16 16:08:07 +0900

    기존 loadDummyData 함수명을 GetDummyData로 변경

--- src/app/main.js
+++ src/app/main.js
@@ -2,7 +2,7 @@ import {Store} from './store.js';
 import {initDateNav} from '../domains/headerDate/ui/dateNav.js';
 import {initSummaryView} from '../domains/summary/ui/summaryView.js';
 import {initFormView} from '../domains/entryForm/ui/formView.js';
-import {loadDummyData} from '../shared/data/dummyData.js';
+import {GetDummyData} from '../shared/data/dummyData.js';
 
 const bootstrap = () => {
     // Store 생성
@@ -21,7 +21,7 @@ const bootstrap = () => {
     initFormView({formEl: document.querySelector('.detail-form'), summaryStore: summaryStore});
 
     // 더미 데이터를 비동기(Promise) 방식으로 로드 후 스토어에 주입
-    loadDummyData()
+    GetDummyData()
         .then(dummy => {
             dummy.forEach(e => summaryStore.dispatch('ENTRY/ADD', e));
         });
