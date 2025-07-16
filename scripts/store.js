fd7f93be907fc8f1b1b906529929e1448428fa22
jjamming <ohj3694@naver.com>
2025-07-16 16:31:24 +0900

    feat: store에 observer 추가

--- scripts/store.js
+++ scripts/store.js
@@ -2,9 +2,16 @@ import { fetchRecords } from "../api/recordsApi.js";
 
 export const store = {
   records: [],
+  observers: [],
+  dateObservers: [],
+  currentDate: {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  },
   init() {
     return fetchRecords().then((res) => {
       this.records = res;
+      this.notify();
     });
   },
   getRecords() {
@@ -12,11 +19,36 @@ export const store = {
   },
   setRecords(newRecords) {
     this.records = newRecords;
+    this.notify();
   },
-  addRecord(record) {
+  addRecordToStore(record) {
     this.records.push(record);
+    this.notify();
   },
-  deleteRecord(itemId) {
-    this.setRecords();
+  deleteRecordToStore(itemId) {
+    // todo: id에 해당하는 레코드 지워서 반영
+
+    this.notify();
+  },
+
+  getDate() {
+    return this.currentDate;
+  },
+  setDate(year, month) {
+    this.currentDate = { year, month };
+    this.notifyDate();
+  },
+  subscribe(callback) {
+    this.observers.push(callback);
+  },
+  subscribeDate(cb) {
+    this.dateObservers.push(cb);
+  },
+
+  notify() {
+    this.observers.forEach((cb) => cb(this.records));
+  },
+  notifyDate() {
+    this.dateObservers.forEach((cb) => cb(this.currentDate));
   },
 };
