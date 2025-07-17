5fa2a1d050a1a1699c614aec730245e1e1de9606
minjoengss <dotoriido@gmail.com>
2025-07-17 15:00:18 +0900

    fix: 숫자 더할 때 문자로 합쳐지는 문제 해결

--- src/utils/Timer.js
+++ src/utils/Timer.js
@@ -1,7 +1,7 @@
 export const Timer = (time, callback) => {
   let startTime = null;
 
-  function step(timestamp) {
+  const step = (timestamp) => {
     if (!startTime) startTime = timestamp;
     const elapsed = timestamp - startTime;
 
@@ -10,7 +10,7 @@ export const Timer = (time, callback) => {
     } else {
       requestAnimationFrame(step);
     }
-  }
+  };
 
   requestAnimationFrame(step);
 };
