dfb7fbaed2f48ce55ec4bb0b29fd0b9954f22c33
jjamming <ohj3694@naver.com>
2025-07-17 16:06:08 +0900

    feat: observer prototype을 활용하는 RecordStore 생성자 함수 정의

---
+++ store/recordStore.js
@@ -0,0 +1,79 @@
+import { Observer } from "./observer.js";
+import { fetchRecords } from "../api/recordsApi.js";
+
+function RecordStore() {
+  Observer.call(this);
+  this.records = [];
+}
+
+RecordStore.prototype = Object.create(Observer.prototype);
+RecordStore.prototype.constructor = RecordStore;
+
+RecordStore.prototype.init = function () {
+  return fetchRecords().then((res) => {
+    this.records = res;
+
+    this.notify();
+  });
+};
+
+RecordStore.prototype.getRecords = function () {
+  console.log(this);
+  return this.records;
+};
+RecordStore.prototype.setRecord = function (newRecords) {
+  this.records = newRecords;
+  this.notify();
+};
+RecordStore.prototype.addRecordToStore = function ({ recordId, date, item }) {
+  // record에 추가하려는 날짜에 대한 정보가 이미 있나 확인
+  const foundRecord = this.records.find((record) => record.date.toString() === date.toString());
+  if (foundRecord) {
+    // 이미 있는 날짜라면 해당 날짜의 items 배열에 추가
+    foundRecord.items.push(item);
+  } else {
+    // 없는 날짜라면 record에 날짜 포함한 새 객체 생성
+    this.records.push({
+      id: recordId,
+      date,
+      items: [item],
+    });
+  }
+  this.notify();
+};
+
+RecordStore.prototype.deleteRecordFromStore = function (dateId, itemId) {
+  this.records = this.records.reduce((acc, record) => {
+    // 삭제하려는 레코드의 날짜의 items 배열에 접근
+    if (record.id.toString() === dateId.toString()) {
+      const filteredItems = record.items.filter((item) => item.id.toString() !== itemId.toString());
+
+      // 삭제 결과 해당 날짜의 item이 남아있다면 남은 items를 반환
+      if (filteredItems.length > 0) {
+        acc.push({ ...record, items: filteredItems });
+      }
+      // item 배열이 없다면 해당 날짜의 객체는 사라짐
+    } else {
+      acc.push(record); // 삭제 대상 날짜가 아니므로 그대로 유지
+    }
+    return acc;
+  }, []);
+
+  this.notify();
+};
+
+RecordStore.prototype.updateRecordInStore = function ({ dateId, itemId, updatedItem }) {
+  this.records = this.records.map((record) => {
+    if (record.id.toString() === dateId.toString()) {
+      return {
+        ...record,
+        items: record.items.map((item) =>
+          item.id.toString() === itemId.toString() ? { ...item, ...updatedItem } : item
+        ),
+      };
+    }
+    return record;
+  });
+  this.notify();
+};
+export const recordStore = new RecordStore();
