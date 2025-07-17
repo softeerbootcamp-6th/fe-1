48c0daef06b2d2b0b9dfb29bbca71886493b23ff
Park Jong Seong <pjsc0903@gmail.com>
2025-07-17 17:28:13 +0900

    월이 바뀔 때(12를 넘어가거나 1보다 작아질 때) 처리하는 함수를 기존의 shift함수 밖으로 빼냄

--- src/domains/headerDate/model/headerDateModel.js
+++ src/domains/headerDate/model/headerDateModel.js
@@ -1,34 +1,44 @@
+const handleShiftingYearMonth = ({ year, month }) => {
+  if (month < 1) {
+    month = 12;
+    --year;
+  }
+  if (month > 12) {
+    month = 1;
+    ++year;
+  }
+  return { year, month };
+};
+
 export class HeaderDateModel {
-    // HeaderDate: 월별 달력 모델
-    constructor(base = new Date()) {
-        // 현재 날짜를 기준으로 연도와 월을 설정
-        this.year = base.getFullYear();
-        this.month = base.getMonth() + 1;
-    }
+  // HeaderDate: 월별 달력 모델
+  constructor(base = new Date()) {
+    // 현재 날짜를 기준으로 연도와 월을 설정
+    this.year = base.getFullYear();
+    this.month = base.getMonth() + 1;
+  }
 
-    // 월을 이동하는 함수
-    shift(d) {
-        this.month += d;
-        if (this.month < 1) {
-            this.month = 12;
-            this.year--;
-        }
-        if (this.month > 12) {
-            this.month = 1;
-            this.year++;
-        }
-    }
+  // 월을 이동하는 함수
+  shift(monthDelta) {
+    this.month += monthDelta;
+    const { year, month } = handleShiftingYearMonth({
+      year: this.year,
+      month: this.month,
+    });
+    this.year = year;
+    this.month = month;
+  }
 
-    // 연도와 월을 설정하는 함수
-    set(y, m) {
-        this.year = y;
-        this.month = m;
-    }
+  // 연도와 월을 설정하는 함수
+  set(y, m) {
+    this.year = y;
+    this.month = m;
+  }
 
-    // 현재 연도와 월을 반환하는 함수
-    get() {
-        return {year: this.year, month: this.month};
-    }
+  // 현재 연도와 월을 반환하는 함수
+  get() {
+    return { year: this.year, month: this.month };
+  }
 }
 
 // template literal을 사용해서 할 수도 있음
