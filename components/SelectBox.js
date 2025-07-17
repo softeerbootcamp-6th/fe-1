6ccc9e80dd97d1610716b8d85b409932e689cd7f
choi-jiae <chlwldo2002@gmail.com>
2025-07-17 18:38:43 +0900

    [feat] select box UI 구현 중
    - 기본 틀 (진행 중)
    - 토글 기능

---
+++ components/SelectBox.js
@@ -0,0 +1,92 @@
+export function renderSelectBox(optionStringList, editable) {
+  let isVisible = false;
+  const selectBox = document.createElement('div');
+  selectBox.className = 'select-box';
+
+  // 가상 DOM 조각
+  const fragment = document.createDocumentFragment();
+
+  // select button
+  const selectButton = document.createElement('button');
+  selectButton.className = 'select-button';
+
+  const selectButtonSpan = document.createElement('span');
+  selectButtonSpan.className = 'select-button-span';
+  selectButtonSpan.textContent = '선택하세요';
+
+  const selectButtonIcon = document.createElement('img');
+  selectButtonIcon.className = 'select-button-icon';
+  selectButtonIcon.src = '../assets/icons/chevron-down.svg';
+
+  selectButton.appendChild(selectButtonSpan);
+  selectButton.appendChild(selectButtonIcon);
+
+  // option list
+  const optionList = document.createElement('ul');
+  optionList.className = 'option-list';
+  optionList.style.display = 'none'; // 초기에는 숨김
+
+  const optionButton = document.createElement('button');
+  optionButton.className = 'option-button';
+
+  selectBox.appendChild(selectButton);
+  selectBox.appendChild(optionList);
+
+  optionStringList.forEach(optionString => {
+    const optionItem = document.createElement('li');
+    optionItem.className = 'option-item';
+
+    const optionItemSpan = document.createElement('span');
+    optionItemSpan.className = 'option-item-span';
+    optionItemSpan.textContent = optionString;
+
+    const optionDelButton = document.createElement('button');
+
+    const optionDelIcon = document.createElement('img');
+    optionDelIcon.src = '../assets/icons/closed.svg';
+    optionDelButton.appendChild(optionDelIcon);
+
+    optionItem.appendChild(optionItemSpan);
+    optionItem.appendChild(optionDelButton);
+
+    fragment.appendChild(optionItem);
+  });
+
+  if (editable) {
+    fragment.appendChild(optionButton);
+  }
+
+  optionList.appendChild(fragment);
+
+  // 옵션 클릭 시 선택 처리
+  optionList.addEventListener('click', e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const selectedOption = e.target.closest('li');
+    selectButton.textContent = selectedOption.textContent;
+    selectBox.value = e.target.textContent;
+    optionList.style.display = 'none';
+
+    // change 이벤트 발생
+    const changeEvent = new Event('change', { bubbles: true });
+    selectBox.dispatchEvent(changeEvent);
+  });
+
+  // 선택 버튼 클릭 시 옵션 리스트 토글
+  selectButton.addEventListener('click', e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    isVisible = !isVisible;
+    optionList.style.display = isVisible ? 'none' : 'flex';
+  });
+
+  // 외부 클릭 시 옵션 리스트 숨기기
+  document.addEventListener('click', e => {
+    if (!selectBox.contains(e.target)) {
+      optionList.style.display = 'none';
+    }
+  });
+
+  return selectBox;
+}
