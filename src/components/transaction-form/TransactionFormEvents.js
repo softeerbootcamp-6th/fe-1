d0169ebe92609c319a0fc3be28b7445bf30a761f
yyeonzu <yyeonzu@gmail.com>
2025-07-17 18:18:02 +0900

    feat: 아이템 추가, 수정 api 연결

--- src/components/transaction-form/TransactionFormEvents.js
+++ src/components/transaction-form/TransactionFormEvents.js
@@ -1,41 +1,92 @@
+import { addItem, getItems, updateItem } from "../../apis/items.js";
 import FormState from "../../store/FormState.js";
 import ItemsState from "../../store/ItemsState.js";
+import { addEventListener } from "../../utils/addEvent.js";
 
 export function addTransactionFormEvents() {
-  const form = document.getElementById("transaction-form");
-  if (!form) return;
-
-  form.addEventListener("input", (e) => {
-    const { name, value } = e.target;
-
-    if (!name) return;
-    FormState.setFormState({ [name]: value });
+  addEventListener({
+    id: "transaction-form",
+    event: "input",
+    onEvent: (e) => {
+      handleChangeInput(e);
+    },
   });
 
-  form.addEventListener("click", (e) => {
-    const toggleBtn = e.target.closest("#transaction-form-toggle-btn");
-    if (!toggleBtn) return;
-
-    const currentType = FormState.getFormState().type;
-    const newType = currentType === "withdraw" ? "deposit" : "withdraw";
-    FormState.setFormState({ type: newType, category: "" });
+  addEventListener({
+    id: "transaction-form",
+    event: "click",
+    onEvent: (e) => {
+      handleClickToggleButton(e);
+    },
   });
 
-  // submit 이벤트 위임 등록
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formState = FormState.getFormState();
-    const newItem = {
-      ...formState,
-      id: formState.editId ?? Date.now(),
-    };
-
-    const prevItems = ItemsState.getItems();
-    const updatedItems = formState.editId
-      ? prevItems.map((item) => (item.id === formState.editId ? newItem : item))
-      : [...prevItems, newItem];
-
-    ItemsState.setItems(updatedItems);
-    FormState.resetFormState();
+  addEventListener({
+    id: "transaction-form",
+    event: "submit",
+    onEvent: (e) => {
+      handleSubmitButton(e);
+    },
   });
+}
+
+// input 변경 이벤트 함수
+function handleChangeInput(e) {
+  const { name, value } = e.target;
+  if (!name) return;
+
+  FormState.setFormState({ [name]: value });
+}
+
+function handleClickToggleButton(e) {
+  const toggleBtn = e.target.closest("#transaction-form-toggle-btn");
+  if (!toggleBtn) return;
+
+  const currentType = FormState.getFormState().type;
+  const newType = currentType === "withdraw" ? "deposit" : "withdraw";
+  FormState.setFormState({ type: newType, category: "" });
+}
+
+// toggle 버튼 변경 이벤트 함수
+
+// form 제출 api 연결
+function handleSubmitButton(e) {
+  e.preventDefault();
+  const formState = FormState.getFormState();
+  const nowItem = {
+    ...formState,
+    id: formState.editId ?? Date.now(),
+  };
+  const isAdd = !Boolean(formState.editId);
+  if (isAdd) submitPost(nowItem);
+  else submitPut(formState.editId, nowItem);
+}
+
+// 추가 함수
+function submitPost(nowItem) {
+  addItem(nowItem)
+    .then((newItem) => {
+      const prevItems = ItemsState.getItems();
+      const updatedItems = [...prevItems, newItem];
+      ItemsState.setItems(updatedItems);
+      FormState.resetFormState();
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+// 수정 함수
+function submitPut(editId, nowItem) {
+  updateItem({ id: editId, updatedItem: nowItem })
+    .then((newItem) => {
+      const prevItems = ItemsState.getItems();
+      const updatedItems = prevItems.map((item) =>
+        item.id === editId ? newItem : item
+      );
+      ItemsState.setItems(updatedItems);
+      FormState.resetFormState();
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
