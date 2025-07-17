d960947b57281688cc6956bfe42f46eff0a9220e
Park Jong Seong <pjsc0903@gmail.com>
2025-07-17 17:30:10 +0900

    초기에 더미데이터를 불러오는 부분을 외부로 뺌

--- src/app/main.js
+++ src/app/main.js
@@ -1,33 +1,36 @@
-import {Store} from './store.js';
-import {initDateNav} from '../domains/headerDate/ui/dateNav.js';
-import {initSummaryView} from '../domains/summary/ui/summaryView.js';
-import {initFormView} from '../domains/entryForm/ui/formView.js';
-import {initDummyData} from "../shared/data/initDummyData.js";
+import { Store } from './store.js';
+import { initDateNav } from '../domains/headerDate/ui/dateNav.js';
+import { initSummaryView } from '../domains/summary/ui/summaryView.js';
+import { initFormView } from '../domains/entryForm/ui/formView.js';
+import { initDummyData } from '../shared/data/initDummyData.js';
 
 const bootstrap = () => {
-    // Store 생성
-    const summaryStore = new Store({entries: []});
-    const dateStore = new Store({year: 2000, month: 9});
-    const selectedEntryStore = new Store({selectedEntry: null});
+  // Store 생성
+  const summaryStore = new Store({ entries: [] });
+  const dateStore = new Store({ year: 2000, month: 9 });
+  const selectedEntryStore = new Store({ selectedEntry: null });
 
-    // 날짜쪽 UI 초기화
-    initDateNav({navEl: document.querySelector('.month-nav'), dateStore: dateStore});
-    // 월별 가계부 UI 초기화
-    initSummaryView({
-        summaryEl: document.querySelector('.month-summary'),
-        summaryStore: summaryStore,
-        dateStore: dateStore,
-        selectedEntryStore: selectedEntryStore
-    });
-    // 입력폼 UI 초기화
-    initFormView({
-        formEl: document.querySelector('.detail-form'),
-        summaryStore: summaryStore,
-        selectedEntryStore: selectedEntryStore
-    });
+  // 날짜쪽 UI 초기화
+  initDateNav({
+    navEl: document.querySelector('.month-nav'),
+    dateStore,
+  });
+  // 월별 가계부 UI 초기화
+  initSummaryView({
+    summaryEl: document.querySelector('.month-summary'),
+    summaryStore,
+    dateStore,
+    selectedEntryStore,
+  });
+  // 입력폼 UI 초기화
+  initFormView({
+    formEl: document.querySelector('.detail-form'),
+    summaryStore,
+    selectedEntryStore,
+  });
 
-    // 더미 데이터를 비동기(Promise) 방식으로 로드 후 스토어에 주입
-    initDummyData({summaryStore});
+  // 더미 데이터를 비동기(Promise) 방식으로 로드 후 스토어에 주입
+  initDummyData({ summaryStore });
 };
 
 document.addEventListener('DOMContentLoaded', bootstrap);
