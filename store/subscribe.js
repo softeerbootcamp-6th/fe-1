a727bdbf8ad515267e96d228883d4a37cb6877ca
jjamming <ohj3694@naver.com>
2025-07-18 14:36:53 +0900

    feat: 헤더 날짜가 변경될 때 현재 탭에 따라 다른 함수가 수행되도록 수정

--- store/subscribe.js
+++ store/subscribe.js
@@ -2,6 +2,8 @@ import { recordStore } from "./recordStore.js";
 import { dateStore } from "./dateStore.js";
 import { renderRecords, renderRecordHeader } from "../scripts/records/recordRender.js";
 import { updateHeaderDateUI } from "../scripts/header.js";
+import { initPage } from "../scripts/router.js";
+import { initCalanderHeader } from "../scripts/calander/calanderUtils.js";
 
 export function subscribeStore() {
   recordStore.subscribe((records) => {
@@ -9,11 +11,21 @@ export function subscribeStore() {
     renderRecordHeader(year, month, records);
     renderRecords(year, month, records);
   });
-
   dateStore.subscribe(({ year, month }) => {
     const records = recordStore.getRecords();
     updateHeaderDateUI(year, month);
-    renderRecordHeader(year, month, records);
-    renderRecords(year, month, records);
+
+    switch (window.location.hash) {
+      case "#main":
+        renderRecordHeader(year, month, records);
+        renderRecords(year, month, records);
+        break;
+      case "#calander":
+        initCalanderHeader();
+        break;
+      default:
+        renderRecordHeader(year, month, records);
+        renderRecords(year, month, records);
+    }
   });
 }
