3983e95461cb529e093721dee39ddd2b215dd2f2
bobob0311 <qweasdzxc0311@naver.com>
2025-07-18 13:33:01 +0900

    함수로 분리

--- components/dailyList/daily.js
+++ components/dailyList/daily.js
@@ -13,27 +13,32 @@ const COLOR = {
     월급: '#E39D5D',
 };
 
-export default function createDaily(dailyInfo) {
-    const { category, description, payment, amount, id } = dailyInfo;
+function createDailyInnerHtml(dailyInfo) {
+    const { category, description, payment, amount } = dailyInfo;
 
-    const dailyInnerHtml = `
-            <div class="category-info lt-14" style="background-color :${
-                COLOR[category]
-            }" >${category}</div>
-            <div class="description-info lt-14">
-                ${description}
-            </div>
-            <div class="payment-info lt-14">${payment}</div>
-            <div class="amount-info ${
-                amount > 0 ? 'text-blue' : 'text-red'
-            } lt-14">${formatAmount(amount)}원</div>
-            <button class="daily-delete-btn sb-12 hidden"> 
-                <div>
-                    <img src=/public/closed.svg/>
-                </div>
-                <span>삭제</span> 
-            </button>
-        `;
+    return `
+    <div class="category-info lt-14" style="background-color :${
+        COLOR[category]
+    }" >${category}</div>
+    <div class="description-info lt-14">
+        ${description}
+    </div>
+    <div class="payment-info lt-14">${payment}</div>
+    <div class="amount-info ${
+        amount > 0 ? 'text-blue' : 'text-red'
+    } lt-14">${formatAmount(amount)}원</div>
+    <button class="daily-delete-btn sb-12 hidden"> 
+        <div>
+            <img src=/public/closed.svg/>
+        </div>
+        <span>삭제</span> 
+    </button>
+    `;
+}
+
+export default function createDaily(dailyInfo) {
+    const { id } = dailyInfo;
+    const dailyInnerHtml = createDailyInnerHtml(dailyInfo);
 
     const $dailyInfo = createElement(
         'li',
