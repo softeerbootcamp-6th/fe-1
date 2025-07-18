4f9f842796bac57c177d60ffc602bd6f69a3c6f8
홍규진 <hkj0206@dgu.ac.kr>
2025-07-18 14:42:36 +0900

    refactor: Statistics 페이지 컴포넌트 분리에 따른 관심사 분리 및 CSS 추가

--- src/pages/statistic/statistic-rendering.js
+++ src/pages/statistic/statistic-rendering.js
@@ -1,5 +1,9 @@
 import { renderCategory } from "./components/category/category-rendering.js";
 import { initCategory } from "./components/category/category-handlers.js";
+import { renderDonutChart } from "./components/donut-chart/donut-chart-rendering.js";
+import { initDonutChart } from "./components/donut-chart/donut-chart-handlers.js";
+import { renderTrendChart } from "./components/trend/trend-rendering.js";
+import { renderHistoryList } from "./components/history/history-rendering.js";
 
 export async function renderStatistic() {
   const statisticHTML = `
@@ -8,22 +12,18 @@ export async function renderStatistic() {
       <!-- 통계 본문 -->
       <div class="statistics-content">
         <!-- 차트 영역 -->
-        <div class="stats-chart">
-          <div id="chart-placeholder"></div>
-        </div>
+        ${renderDonutChart()}
         
         <!-- 카테고리별 상세 정보 -->
-        <div class="stats-categories">
-          ${renderCategory()}
-        </div>
+        ${renderCategory()}
       </div>
     </div>
     
     <!-- 카테고리별 월별 지출 추이 차트 -->
-    <div class="trend-chart" id="trend-chart"></div>
+    ${renderTrendChart()}
 
     <!-- 선택된 카테고리별 월별 지출 추이 차트 -->
-    <div class="history-list" id="history-list"></div>
+    ${renderHistoryList()}
   `;
 
   return statisticHTML;
@@ -31,12 +31,14 @@ export async function renderStatistic() {
 
 // 자동으로 통계 렌더링 및 초기화
 document.addEventListener("DOMContentLoaded", function () {
-  const bodyContainer = document.getElementById("body-container");
-  if (bodyContainer) {
+  const statisticContainer = document.getElementById("statistic-container");
+  if (statisticContainer) {
     renderStatistic().then((html) => {
-      bodyContainer.innerHTML = html;
       // 카테고리 컴포넌트 초기화
+      statisticContainer.innerHTML = html;
+      // 컴포넌트 초기화
       initCategory();
+      initDonutChart();
     });
   }
 });
