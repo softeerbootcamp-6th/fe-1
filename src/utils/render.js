8fc63a4c24e1eef91c18cbe64e64fa59aee3a4e0
yyeonzu <yyeonzu@gmail.com>
2025-07-18 12:20:41 +0900

    refactor: 찾는 요소가 없을 경우 렌더링하지 않고  리턴하도록 가드 추가

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,5 +1,6 @@
 export function renderComponent({ id, className, innerHTML }) {
   const content = document.getElementById(id);
+  if (!content) return;
   content.className = className;
   content.innerHTML = innerHTML;
 }
