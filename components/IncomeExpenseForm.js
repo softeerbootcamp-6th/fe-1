dd80f923cd1bbb69c42886639f342048ad1774ba
choi-jiae <chlwldo2002@gmail.com>
2025-07-19 13:49:02 +0900

    [fix] form입력 및 list 업데이트 오류 수정
    - form 초기화 시 select box 초기화 안되는 문제 해결
    - select box 선택 시 validate 안되는 문제 해결

--- components/IncomeExpenseForm.js
+++ components/IncomeExpenseForm.js
@@ -1,39 +1,39 @@
-import { incomeExpenseStore } from '../store/incomeExpenseStore.js';
-import { renderListItem } from './IncomeExpenseList.js';
-import { renderSelectBox } from './SelectBox.js';
+import { incomeExpenseStore } from "../store/incomeExpenseStore.js";
+import { renderListItem } from "./IncomeExpenseList.js";
+import { renderSelectBox } from "./SelectBox.js";
 
 export function renderIncomeExpenseForm() {
-  const form = document.createElement('form');
-  form.className = 'income-expense-form';
+  const form = document.createElement("form");
+  form.className = "income-expense-form";
 
   // 현재 날짜를 기본값으로 설정
   const today = new Date();
-  const formattedDate = today.toISOString().split('T')[0];
+  const formattedDate = today.toISOString().split("T")[0];
   let isEdit = false;
   let isIncome = true; // true: 수입, false: 지출
   let itemID = null; // list item id 초기화 -> list item 클릭하여 수정하는 경우에만 다른 숫자 가능
   const maxDescriptionLength = 32;
   let descriptionLength = 0;
-  const paymentOptions = ['현금', '신용카드'];
-  const incomeTags = ['월급', '용돈', '기타 수입'];
+  const paymentOptions = ["현금", "신용카드"];
+  const incomeTags = ["월급", "용돈", "기타 수입"];
   const expenseTags = [
-    '생활',
-    '의료/건강',
-    '쇼핑/뷰티',
-    '교통',
-    '식비',
-    '문화/여가',
-    '미분류',
+    "생활",
+    "의료/건강",
+    "쇼핑/뷰티",
+    "교통",
+    "식비",
+    "문화/여가",
+    "미분류",
   ];
 
-  document.addEventListener('add-payment', e => {
+  document.addEventListener("add-payment", (e) => {
     const paymentName = e.detail.paymentName;
     paymentOptions.push(paymentName);
 
     updatePaymentSelect(paymentOptions);
   });
 
-  document.addEventListener('delete-payment', e => {
+  document.addEventListener("delete-payment", (e) => {
     const optionName = e.detail.optionName;
     const index = paymentOptions.indexOf(optionName);
     if (index > -1) {
@@ -66,7 +66,7 @@ export function renderIncomeExpenseForm() {
     `;
   };
 
-  const getDescriptionContainerHTML = maxDescriptionLength => {
+  const getDescriptionContainerHTML = (maxDescriptionLength) => {
     return `
         <div class="description-container">
             <div class="description-label-container">
@@ -121,86 +121,84 @@ export function renderIncomeExpenseForm() {
     ${addButtonHTML()}
     `;
 
-  const paymentContainer = form.querySelector('.payment-container');
+  const paymentContainer = form.querySelector(".payment-container");
   let selectBox = renderSelectBox(paymentOptions, true, true);
   paymentContainer.appendChild(selectBox);
 
-  const tagContainer = form.querySelector('.tag-container');
+  const tagContainer = form.querySelector(".tag-container");
   const incomeExpenseTags = isIncome ? incomeTags : expenseTags;
   let tagSelectBox = renderSelectBox(incomeExpenseTags, false, false);
   tagContainer.appendChild(tagSelectBox);
 
   // 날짜
-  const dateInput = form.querySelector('.date-input');
+  const dateInput = form.querySelector(".date-input");
 
   // 금액
-  const moneyButton = form.querySelector('.money-button');
-  const moneyButtonIcon = moneyButton.querySelector('img');
-  const moneyInput = form.querySelector('#money-input');
+  const moneyButton = form.querySelector(".money-button");
+  const moneyButtonIcon = moneyButton.querySelector("img");
+  const moneyInput = form.querySelector("#money-input");
 
   // 내용
-  const descriptionInput = form.querySelector('#description-input');
-
-  // 결제수단
-  let paymentSelect = form.querySelector('.select-box'); // 첫 번째 select-box
-
-  // 분류
-  let tagSelectSpan = form.querySelectorAll('.select-button-span')[1]; // 두 번째 select-box
+  const descriptionInput = form.querySelector("#description-input");
 
   // add 버튼
-  const addButton = form.querySelector('.add-button');
+  const addButton = form.querySelector(".add-button");
 
-  moneyButton.addEventListener('click', e => {
+  moneyButton.addEventListener("click", (e) => {
     e.stopPropagation();
     e.preventDefault();
     isIncome = !isIncome;
     moneyButtonIcon.src = isIncome
-      ? '../assets/icons/plus.svg'
-      : '../assets/icons/minus.svg';
+      ? "../assets/icons/plus.svg"
+      : "../assets/icons/minus.svg";
 
     // 클래스 옵션 업데이트
     updateTagSelect(incomeTags, expenseTags);
   });
 
-  moneyInput.addEventListener('keyup', ({ target }) => {
-    let moneyValue = Number(target.value.replace(/[^0-9]/g, '')); // 숫자가 아닌 것들은 제외
-    let formattedValue = moneyValue.toLocaleString('ko-KR'); // 세자리마다 콤마
+  moneyInput.addEventListener("keyup", ({ target }) => {
+    let moneyValue = Number(target.value.replace(/[^0-9]/g, "")); // 숫자가 아닌 것들은 제외
+    let formattedValue = moneyValue.toLocaleString("ko-KR"); // 세자리마다 콤마
     target.value = formattedValue; // 입력 필드에 포맷된 값 설정
   });
 
-  form.addEventListener('click', e => {
+  form.addEventListener("click", (e) => {
     e.stopPropagation();
   });
 
   // 입력 시 기본값 0 제거
-  moneyInput.addEventListener('focus', ({ target }) => {
-    if (target.value === '0') {
-      target.value = '';
+  moneyInput.addEventListener("focus", ({ target }) => {
+    if (target.value === "0") {
+      target.value = "";
     }
   });
 
-  descriptionInput.addEventListener('keyup', ({ target }) => {
+  descriptionInput.addEventListener("keyup", ({ target }) => {
     descriptionLength = target.value.length;
-    const lengthSpan = form.querySelector('.description-length');
+    const lengthSpan = form.querySelector(".description-length");
     lengthSpan.textContent = `${descriptionLength}/32`;
   });
 
   // keyup, select 이벤트 발생 시 유효성 검사
-  [dateInput, moneyInput, descriptionInput].forEach(input => {
-    input.addEventListener('keyup', () => {
+  [dateInput, moneyInput, descriptionInput].forEach((input) => {
+    input.addEventListener("keyup", () => {
       validateForm(dateInput, moneyInput, descriptionInput);
     });
-    input.addEventListener('change', () => {
+    input.addEventListener("change", () => {
       validateForm(dateInput, moneyInput, descriptionInput);
     });
   });
 
-  addButton.addEventListener('click', e => {
+  document.addEventListener("select-change", () => {
+    validateForm(dateInput, moneyInput, descriptionInput);
+  });
+
+  addButton.addEventListener("click", (e) => {
     e.stopPropagation();
     e.preventDefault();
 
     const incomeExpenseListContainer = document.querySelector(
-      '.income-expense-list-container'
+      ".income-expense-list-container",
     );
 
     handleSubmit(e, itemID, dateInput, moneyInput, descriptionInput);
@@ -209,7 +207,7 @@ export function renderIncomeExpenseForm() {
     renderListItem(incomeExpenseListContainer);
   });
 
-  document.addEventListener('edit-item', e => {
+  document.addEventListener("edit-item", (e) => {
     const data = e.detail;
     isEdit = true;
     setItemToForm(data);
@@ -218,9 +216,9 @@ export function renderIncomeExpenseForm() {
   // 내가 클릭한 것이 아닌지 확인할 수 있으면 됨
   // 생각을 거꾸로
   // 바깔부분 클릭하면 edit 해제
-  document.body.addEventListener('click', event => {
+  document.body.addEventListener("click", (event) => {
     // 특정 요소 안에서 클릭했는지 확인
-    const isInsideList = event.target.closest('li');
+    const isInsideList = event.target.closest("li");
     if (!isInsideList && isEdit) {
       formInit(dateInput, moneyInput, descriptionInput);
       isEdit = false;
@@ -228,7 +226,7 @@ export function renderIncomeExpenseForm() {
   });
 
   const updateTagSelect = (incomeTags, expenseTags) => {
-    const oldSelectBox = tagContainer.querySelector('.select-box');
+    const oldSelectBox = tagContainer.querySelector(".select-box");
     if (oldSelectBox) {
       tagContainer.removeChild(oldSelectBox);
     }
@@ -238,41 +236,35 @@ export function renderIncomeExpenseForm() {
     } else {
       tagContainer.appendChild(renderSelectBox(expenseTags, false, false));
     }
-
-    // 새로운 tagSelect에 이벤트 리스너 추가
-    tagSelectSpan.addEventListener('change', () => {
-      validateForm(dateInput, moneyInput, descriptionInput);
-    });
   };
 
-  const updatePaymentSelect = options => {
-    const oldSelectBox = paymentContainer.querySelector('.select-box');
+  const updatePaymentSelect = (options) => {
+    const oldSelectBox = paymentContainer.querySelector(".select-box");
     if (oldSelectBox) {
       paymentContainer.removeChild(oldSelectBox);
     }
     paymentContainer.appendChild(renderSelectBox(options, true, true));
-    const newSelectBox = paymentContainer.querySelector('.select-box');
-    newSelectBox.addEventListener('change', () => {
+    const newSelectBox = paymentContainer.querySelector(".select-box");
+    newSelectBox.addEventListener("change", () => {
       validateForm(dateInput, moneyInput, descriptionInput);
     });
   };
 
   // 입력값 유효성 검사
   const validateForm = (dateInput, moneyInput, descriptionInput) => {
-    const paymentSelect = form.querySelector('.select-box'); // 첫 번째 select-box
-    const tagSelectSpan = form.querySelectorAll('.select-button-span')[1]; // 두 번째 select-box
+    const paymentSelect = form.querySelector(".select-box");
+    const tagSelect = form.querySelectorAll(".select-box")[1];
+
     // 날짜 입력값 확인
     const isDateValid = !!dateInput.value; // 날짜가 선택되었는지 확인(true/false 강제변환)
     // 금액 입력값 확인 (0 보다 큰가)
-    const isMoneyValid = Number(moneyInput.value.replace(/[^0-9]/g, '')) > 0;
+    const isMoneyValid = Number(moneyInput.value.replace(/[^0-9]/g, "")) > 0;
     // 내용 입력값 확인 (1글자 이상)
     const isDescriptionValid = descriptionInput.value.trim().length > 0;
     // 결제수단 선택 여부 확인
-    const isPaymentValid = paymentSelect.textContent !== '';
+    const isPaymentValid = paymentSelect.dataset.value !== "";
     // 분류 선택 여부 확인
-    const isTagValid = tagSelectSpan.textContent !== '';
-    console.log(paymentSelect);
-    console.log(paymentSelect.dataset.value);
+    const isTagValid = tagSelect.dataset.value !== "";
 
     const isValid =
       isDateValid &&
@@ -284,29 +276,29 @@ export function renderIncomeExpenseForm() {
     activeAddButton(isValid);
   };
 
-  const activeAddButton = isValid => {
+  const activeAddButton = (isValid) => {
     if (isValid) {
-      addButton.classList.toggle('active', isValid);
+      addButton.classList.toggle("active", isValid);
     } else {
-      addButton.classList.remove('active');
+      addButton.classList.remove("active");
     }
   };
 
   const handleSubmit = (e, ID, dateInput, moneyInput, descriptionInput) => {
     e.preventDefault();
 
-    const paymentSelect = form.querySelector('.select-box'); // 첫 번째 select-box
-    const tagSelectSpan = form.querySelectorAll('.select-button-span')[1]; // 두 번째 select-box
+    const paymentSelect = form.querySelector(".select-box");
+    const tagSelect = form.querySelectorAll(".select-box")[1];
 
     const dateInputValue = dateInput.value;
-    const moneyInputValue = moneyInput.value.replace(/[^0-9]/g, ''); // 숫자만 추출
+    const moneyInputValue = moneyInput.value.replace(/[^0-9]/g, ""); // 숫자만 추출
     const descriptionInputValue = descriptionInput.value.trim();
     const paymentSelectValue = paymentSelect.dataset.value;
-    const tagSelectValue = tagSelectSpan.dataset.value;
+    const tagSelectValue = tagSelect.dataset.value;
 
     const newIncomeExpense = {
       id: ID,
-      type: isIncome ? 'income' : 'expense',
+      type: isIncome ? "income" : "expense",
       money: isIncome ? Number(moneyInputValue) : -Number(moneyInputValue),
       description: descriptionInputValue,
       payment: paymentSelectValue,
@@ -315,24 +307,24 @@ export function renderIncomeExpenseForm() {
 
     incomeExpenseStore.updateIncomeExpenseData(
       dateInputValue,
-      newIncomeExpense
+      newIncomeExpense,
     );
   };
 
-  const formInit = (
-    dateInput,
-    moneyInput,
-    descriptionInput,
-    paymentSelect,
-    tagSelectSpan
-  ) => {
+  const formInit = (dateInput, moneyInput, descriptionInput) => {
+    const paymentSelect = form.querySelector(".select-box");
+    const tagSelect = form.querySelectorAll(".select-box")[1];
+
     itemID = null;
     dateInput.value = formattedDate;
-    moneyInput.value = '';
-    descriptionInput.value = '';
+    moneyInput.value = "";
+    descriptionInput.value = "";
 
-    paymentSelect.textContent = '선택하세요';
-    tagSelectSpan.textContent = '선택하세요';
+    paymentSelect.dataset.value = "";
+    tagSelect.dataset.value = "";
+    paymentSelect.querySelector(".select-button-span").textContent =
+      "선택하세요";
+    tagSelect.querySelector(".select-button-span").textContent = "선택하세요";
     activeAddButton(false);
   };
 
@@ -345,22 +337,26 @@ export function renderIncomeExpenseForm() {
     payment,
     tag,
   }) => {
-    isIncome = type === 'income' ? true : false;
+    isIncome = type === "income" ? true : false;
     moneyButtonIcon.src = isIncome
-      ? '../assets/icons/plus.svg'
-      : '../assets/icons/minus.svg';
+      ? "../assets/icons/plus.svg"
+      : "../assets/icons/minus.svg";
     updateTagSelect(incomeTags, expenseTags);
     itemID = id;
     dateInput.value = targetListItemDate;
     moneyInput.value = money > 0 ? money : -money;
     descriptionInput.value = description;
 
-    const lengthSpan = form.querySelector('.description-length');
+    const lengthSpan = form.querySelector(".description-length");
     descriptionLength = description.length;
     lengthSpan.textContent = `${descriptionLength}/32`;
-    paymentSelect.textContent = payment;
-    console.log(tagSelectSpan);
-    tagSelectSpan.textContent = tag;
+
+    const paymentSelect = form.querySelector(".select-box");
+    const tagSelect = form.querySelectorAll(".select-box")[1];
+    paymentSelect.dataset.value = payment;
+    paymentSelect.querySelector(".select-button-span").textContent = payment;
+    tagSelect.querySelector(".select-button-span").textContent = tag;
+    tagSelect.dataset.value = tag;
   };
 
   return form;
--- components/SelectBox.js
+++ components/SelectBox.js
@@ -1,48 +1,48 @@
 export function renderSelectBox(optionStringList, editable, deletable) {
   console.log(optionStringList);
-  const selectBox = document.createElement('div'); //div value설정이 안됨. dataset.value로 설정
-  selectBox.className = 'select-box';
+  const selectBox = document.createElement("div"); //div value설정이 안됨. dataset.value로 설정
+  selectBox.className = "select-box";
 
   // 가상 DOM 조각
   const fragment = document.createDocumentFragment();
 
   // select button
-  const selectButton = document.createElement('button');
-  selectButton.className = 'select-button';
+  const selectButton = document.createElement("button");
+  selectButton.className = "select-button";
 
-  const selectButtonSpan = document.createElement('span');
-  selectButtonSpan.className = 'select-button-span semibold12';
-  selectButtonSpan.textContent = '선택하세요';
+  const selectButtonSpan = document.createElement("span");
+  selectButtonSpan.className = "select-button-span semibold12";
+  selectButtonSpan.textContent = "선택하세요";
 
-  const selectButtonIcon = document.createElement('img');
-  selectButtonIcon.className = 'select-button-icon';
-  selectButtonIcon.src = '../assets/icons/chevron-down.svg';
-  selectButtonIcon.style.width = '1rem';
-  selectButtonIcon.style.height = '1rem';
+  const selectButtonIcon = document.createElement("img");
+  selectButtonIcon.className = "select-button-icon";
+  selectButtonIcon.src = "../assets/icons/chevron-down.svg";
+  selectButtonIcon.style.width = "1rem";
+  selectButtonIcon.style.height = "1rem";
 
   selectButton.appendChild(selectButtonSpan);
   selectButton.appendChild(selectButtonIcon);
 
   // option list
-  const optionList = document.createElement('ul');
-  optionList.className = 'option-list';
+  const optionList = document.createElement("ul");
+  optionList.className = "option-list";
 
   selectBox.appendChild(selectButton);
   selectBox.appendChild(optionList);
 
-  optionStringList.forEach(optionString => {
-    const optionItem = document.createElement('li');
-    optionItem.className = 'option-item';
+  optionStringList.forEach((optionString) => {
+    const optionItem = document.createElement("li");
+    optionItem.className = "option-item";
 
-    const optionItemSpan = document.createElement('span');
-    optionItemSpan.className = 'option-item-span';
+    const optionItemSpan = document.createElement("span");
+    optionItemSpan.className = "option-item-span";
     optionItemSpan.textContent = optionString;
 
-    const optionDelButton = document.createElement('button');
-    optionDelButton.className = 'option-del-button';
+    const optionDelButton = document.createElement("button");
+    optionDelButton.className = "option-del-button";
 
-    const optionDelIcon = document.createElement('img');
-    optionDelIcon.src = '../assets/icons/closed.svg';
+    const optionDelIcon = document.createElement("img");
+    optionDelIcon.src = "../assets/icons/closed.svg";
     optionDelButton.appendChild(optionDelIcon);
 
     optionItem.appendChild(optionItemSpan);
@@ -52,9 +52,9 @@ export function renderSelectBox(optionStringList, editable, deletable) {
     fragment.appendChild(optionItem);
   });
 
-  const optionButton = document.createElement('button');
-  optionButton.className = 'option-button';
-  optionButton.textContent = '추가하기';
+  const optionButton = document.createElement("button");
+  optionButton.className = "option-button";
+  optionButton.textContent = "추가하기";
 
   if (editable) {
     fragment.appendChild(optionButton);
@@ -63,17 +63,17 @@ export function renderSelectBox(optionStringList, editable, deletable) {
   optionList.appendChild(fragment);
 
   // 옵션 클릭 시 선택 처리
-  optionList.addEventListener('click', e => {
+  optionList.addEventListener("click", (e) => {
     e.preventDefault();
     e.stopPropagation();
 
     // 삭제 버튼 클릭 시
-    if (e.target.closest('.option-del-button')) {
-      const optionItem = e.target.closest('.option-item');
+    if (e.target.closest(".option-del-button")) {
+      const optionItem = e.target.closest(".option-item");
       if (optionItem) {
         const optionItemText =
-          optionItem.querySelector('.option-item-span').textContent;
-        const optionDeleteEvent = new CustomEvent('delete-option', {
+          optionItem.querySelector(".option-item-span").textContent;
+        const optionDeleteEvent = new CustomEvent("delete-option", {
           detail: {
             optionName: optionItemText,
           },
@@ -85,41 +85,40 @@ export function renderSelectBox(optionStringList, editable, deletable) {
     }
 
     // 추가하기 버튼 클릭 시
-    if (e.target.closest('.option-button')) {
-      // 여기에 추가 로직 구현 가능
-      const addModalOpenEvent = new Event('add-modal-open');
+    if (e.target.closest(".option-button")) {
+      const addModalOpenEvent = new Event("add-modal-open");
       document.dispatchEvent(addModalOpenEvent);
       return;
     }
 
     // 옵션 아이템 클릭 시 선택 처리
-    const selectedOption = e.target.closest('.option-item');
+    const selectedOption = e.target.closest(".option-item");
     if (selectedOption) {
-      const optionSpan = selectedOption.querySelector('.option-item-span');
+      const optionSpan = selectedOption.querySelector(".option-item-span");
       if (optionSpan) {
         selectButtonSpan.textContent = optionSpan.textContent;
         selectBox.dataset.value = optionSpan.textContent;
-        console.log('선택된 값:', selectBox);
-        optionList.classList.remove('active');
+        console.log("선택된 값:", selectBox);
+        optionList.classList.remove("active");
 
         // change 이벤트 발생
-        const changeEvent = new Event('change', { bubbles: true });
-        selectBox.dispatchEvent(changeEvent);
+        const changeEvent = new Event("select-change", { bubbles: true });
+        document.dispatchEvent(changeEvent);
       }
     }
   });
 
   // 선택 버튼 클릭 시 옵션 리스트 토글
-  selectButton.addEventListener('click', e => {
+  selectButton.addEventListener("click", (e) => {
     e.preventDefault();
     e.stopPropagation();
-    optionList.classList.toggle('active');
+    optionList.classList.toggle("active");
   });
 
   // 외부 클릭 시 옵션 리스트 숨기기
-  document.addEventListener('click', e => {
+  document.addEventListener("click", (e) => {
     if (!selectBox.contains(e.target)) {
-      optionList.classList.remove('active');
+      optionList.classList.remove("active");
     }
   });
 
